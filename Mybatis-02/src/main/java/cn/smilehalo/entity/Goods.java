a162377560c069de4dc7e45131fe134a9ecb041b
1509589130@qq.com <45083988+smilecj@users.noreply.github.com>
2021-11-08 17:27:52 +0800

    一对多对象关联查询

--- Mybatis-02/src/main/java/cn/smilehalo/entity/Goods.java
+++ Mybatis-02/src/main/java/cn/smilehalo/entity/Goods.java
@@ -1,5 +1,7 @@
 package cn.smilehalo.entity;
 
+import java.util.List;
+
 public class Goods {
     private Integer goodsId; //商品编号
     private String title;       //标题
@@ -9,6 +11,15 @@ public class Goods {
     private Float discount;     //则扣率
     private Integer isFreeDelivery;   //是否包邮，1-包邮，0-不包
     private Integer categoryId;  // 分类编号
+    private List<GoodsDetail> goodsDetails;
+
+    public List<GoodsDetail> getGoodsDetails() {
+        return goodsDetails;
+    }
+
+    public void setGoodsDetails(List<GoodsDetail> goodsDetails) {
+        this.goodsDetails = goodsDetails;
+    }
 
     public Goods() {
     }
