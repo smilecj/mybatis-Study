a27735fadc3076a376ddcbb27752d1fab53f1c33
1509589130@qq.com <45083988+smilecj@users.noreply.github.com>
2021-11-12 17:10:35 +0800

    mybatis-annotation 注解开发

--- mybatis-annotation/src/main/java/cn/smilehalocj/dao/GoodsDAO.java
+++ mybatis-annotation/src/main/java/cn/smilehalocj/dao/GoodsDAO.java
@@ -1,12 +1,19 @@
 package cn.smilehalocj.dao;
 
 import cn.smilehalocj.entity.Goods;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 
 import java.util.List;
 
 public interface GoodsDAO {
     @Select ("select * from t_goods where current_price between #{min} and #{max} order by current_price limit 0,#{limt}")
     public List<Goods> selectByPriceRange(@Param("min") Float min, @Param("max") Float max, @Param("limt") Integer limt);
+
+    @Insert(" INSERT INTO t_goods(title,sub_title,original_cost,current_price,discount,is_free_delivery,category_id) VALUE(#{title},#{subTitle},#{originalCost},#{currentPrice},#{discount},#{isFreeDelivery},#{categoryId});")
+    //<SelectKey>
+    @SelectKey(statement = "select last_insert_id()",before = false, keyProperty = "goodsId",resultType = Integer.class)
+    public int insert(Goods goods);
 }
--- mybatis-annotation/src/test/java/cn/smilehalocj/MyBatisTestor.java
+++ mybatis-annotation/src/test/java/cn/smilehalocj/MyBatisTestor.java
@@ -23,4 +23,71 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(sqlSession);
         }
     }
+
+    @Test
+    public void testInsert() throws Exception{
+        SqlSession sqlSession = null;
+        try {
+            sqlSession  = MyBatisUtils.openSession();
+            Goods goods = new Goods();
+            goods.setTitle("测试商品");
+            goods.setTitle("测试子标题");
+            goods.setOriginalCost(200f);
+            goods.setCurrentPrice(100f);
+            goods.setDiscount(0.5f);
+            goods.setIsFreeDelivery(1);
+            goods.setCategoryId(43);
+            GoodsDAO goodsDAO = sqlSession.getMapper(GoodsDAO.class);
+            int num = goodsDAO.insert(goods);
+            sqlSession.commit();
+            System.out.println(goods.getGoodsId());
+
+
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     *  测试模板
+     * @throws Exception
+     */
+    @Test
+    public void MUBan() throws Exception{
+        SqlSession sqlSession = null;
+        try {
+            sqlSession  = MyBatisUtils.openSession();
+            GoodsDAO goodsDAO = sqlSession.getMapper(GoodsDAO.class);
+            sqlSession.commit();
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+
+
 }
